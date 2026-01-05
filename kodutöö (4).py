1c9eba70833f25b40c557786986d9e5999a200f7
hkilumets-a11y <hkilumets@hkhk.edu.ee>
2026-01-05 21:42:38 +0200

    1.5.26

---
+++ kodutöö (4).py
@@ -0,0 +1,70 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+
+
+t.penup()
+t.goto(0, 0)
+t.setheading(0)
+t.pendown()
+
+# 1. ruut
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+
+t.left(72)
+
+# 2. ruut
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+
+t.left(72)
+
+# 3. ruut
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+
+t.left(72)
+
+# 4. ruut
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+
+t.left(72)
+
+# 5. ruut
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+
+turtle.done()
