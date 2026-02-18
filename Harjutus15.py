b1ffeeecdb47643898051bf1a04273237916c0e6
hkilumets-a11y <hkilumets@hkhk.edu.ee>
2026-02-18 11:46:06 +0200

    harjutus

--- Harjutus15.py
+++ Harjutus15.py
@@ -57,7 +57,7 @@ def peegelda_porkumisel():
     y = ristkylik.ycor()
     if (ring.ycor() <= y and ring.ycor()+5 >= y) and (ring.xcor() <= x and ring.xcor()+100 >= x):
         skoor+=1
-        tuple.hideturtle()
+        turtle.hideturtle()
         turtle.clear()
         turtle.penup()
         turtle.goto(-250, 200)
