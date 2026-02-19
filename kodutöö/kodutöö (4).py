823f48f29863ff7cd1e9aa5e93700fdedd84ac93
hkilumets-a11y <hkilumets@hkhk.edu.ee>
2026-02-19 10:34:05 +0200

    kodutöö

--- kodutöö/kodutöö (4).py
+++ kodutöö/kodutöö (4).py
@@ -1,70 +1,16 @@
 import turtle
 
-t = turtle.Turtle()
-t.speed(0)
+ 
+turtle.speed(0)
 
 
-t.penup()
-t.goto(0, 0)
-t.setheading(0)
-t.pendown()
+for _ in range(5):
+    
+    for _ in range(4):
+        turtle.forward(200)
+        turtle.left(90)
+    
 
-# 1. ruut
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-
-t.left(72)
-
-# 2. ruut
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-
-t.left(72)
-
-# 3. ruut
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-
-t.left(72)
-
-# 4. ruut
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-
-t.left(72)
-
-# 5. ruut
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
+    turtle.left(72)
 
 turtle.done()
